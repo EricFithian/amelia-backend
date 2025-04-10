18306e4d602241f45b10e6dfb89e1fbbb634fa1a
EricFithian <ef2413@columbia.edu>
2025-04-10 00:56:53 -0400

    Updated schema

--- models/Appointments.js
+++ models/Appointments.js
@@ -3,6 +3,68 @@ const mongoose = require("mongoose");
 ///////////////////////////////
 // MODELS
 ////////////////////////////////
+const identifierSchema = new mongoose.Schema({
+  system: {type: String, default: "urn:oid:1.2.840.114350.1.13.861.1.7.3.698084.8"},
+  value: {type: String, default: "10001659236"}
+})
+
+const serviceCodingSchema = new mongoose.Schema({
+  system: {type: String, default: "http://open.epic.com/FHIR/StructureDefinition/appointment-service-category"},
+  code: {type: String, default: "appointment"},
+  display: {type: String, default: "Appointment"}
+})
+
+const serviceTypeCodingSchema = new mongoose.Schema({
+    system: {type: String, default: "urn:oid:1.2.840.114350.1.13.861.1.7.2.808267"},
+    code: {type: String, default: "579"},
+    display: {type: String, default: "ABF Office Visit"}
+})
+
+const appointmentTypeCodingSchema = new mongoose.Schema(
+  {
+    system: {type: String, default: "http://hl7.org/fhir/v3/ActCode"},
+    code: {type: String, default: "AMB"},
+    display: {type: String, default: "Ambulatory"}
+  }
+)
+
+const appointmentTypeReasonSchema = new mongoose.Schema(
+  {
+    coding: [appointmentTypeReasonCodingSchema],
+    text: {type: String, default: "Abnormal head movements"}
+  }
+)
+
+const appointmentTypeReasonCodingSchema = new mongoose.Schema(
+  {
+    system: {type: String, default: "http://snomed.info/sct"},
+    code: {type: String, default: "271799000"},
+    display: {type: String, default: "Head movements abnormal (finding)"}
+  }
+)
+
+const participantSchema = new mongoose.Schema(
+  {
+    actor: {
+      reference: {type: String, default: "https://hostname/instance/api/FHIR/STU3/Patient/eFs2zvgmbGfgWFfHliSRYZA3"},
+      display: {type: String, default: "Eric Fithian"}
+    },
+    status: {type: String, default: "tentative"}
+  }, {
+    actor: {
+      reference: {type: String, default: "https://hostname/instance/api/FHIR/STU3/Patient/eFs2zvgmbGfgWFfHliSRYZA3"},
+      display: {type: String, default: "Amanda Fahr"}
+    },
+    status: "tentative"
+  }, {
+    actor: {
+      reference: {type: String, default: "https://hostname/instance/api/FHIR/STU3/Patient/eFs2zvgmbGfgWFfHliSRYZA3"},
+      display: {type: String, default: "ABF Family Practice"}
+    },
+    status: "tentative"
+  }, 
+)
+
 const AppointmentsSchema = new mongoose.Schema({
   resourceType: {
     type: String,
@@ -10,79 +72,26 @@ const AppointmentsSchema = new mongoose.Schema({
   }, id: {
     type: String,
     default: "ePjxkyjA8gju08Vwqc.iiAFHBGCmkucuk3O15LOr0KFg3"
-  }, identifier: [
-    {
-      system: {type: String, default: "urn:oid:1.2.840.114350.1.13.861.1.7.3.698084.8"},
-      value: {type: String, default: "10001659236"}
-    }
-  ], status: {
+  }, identifier: [identifierSchema], 
+  status: {
     type: String,
     default: "proposed"
   }, serviceCategory: {
-    coding: [
-      {
-        system: {type: String, default: "http://open.epic.com/FHIR/StructureDefinition/appointment-service-category"},
-        code: {type: String, default: "appointment"},
-        display: {type: String, default: "Appointment"}
-      }
-    ],
+    coding: [serviceCodingSchema],
     text: {type: String, default: "appointment"}
   }, serviceType: [
     {
-      coding: [
-        {
-          system: {type: String, default: "urn:oid:1.2.840.114350.1.13.861.1.7.2.808267"},
-          code: {type: String, default: "579"},
-          display: {type: String, default: "ABF Office Visit"}
-        }
-      ]
+      coding: [serviceTypeCodingSchema]
     }
   ],
   appointmentType: {
-    coding: [
-      {
-        system: {type: String, default: "http://hl7.org/fhir/v3/ActCode"},
-        code: {type: String, default: "AMB"},
-        display: {type: String, default: "Ambulatory"}
-      }
-    ]
-  }, reason: [
-    {
-      coding: [
-        {
-          system: {type: String, default: "http://snomed.info/sct"},
-          code: {type: String, default: "271799000"},
-          display: {type: String, default: "Head movements abnormal (finding)"}
-        }
-      ],
-      text: {type: String, default: "Abnormal head movements"}
-    }
-  ],
+    coding: [appointmentTypeCodingSchema]
+  }, reason: [appointmentTypeReasonSchema],
   start: {type: String, default: "2021-08-22T13:15:00Z"},
   end: {type: String, default: "2021-08-22T13:30:00Z"},
   minutesDuration: {type: Number, default: 15},
   comment: {type: String, default: "Patient instructions are spiffy!\n\nThis will get you to google!\n"},
-  participant: [
-    {
-      actor: {
-        reference: {type: String, default: "https://hostname/instance/api/FHIR/STU3/Patient/eFs2zvgmbGfgWFfHliSRYZA3"},
-        display: {type: String, default: "Eric Fithian"}
-      },
-      status: {type: String, default: "tentative"}
-    }, {
-      actor: {
-        reference: {type: String, default: "https://hostname/instance/api/FHIR/STU3/Patient/eFs2zvgmbGfgWFfHliSRYZA3"},
-        display: {type: String, default: "Amanda Fahr"}
-      },
-      status: "tentative"
-    }, {
-      actor: {
-        reference: {type: String, default: "https://hostname/instance/api/FHIR/STU3/Patient/eFs2zvgmbGfgWFfHliSRYZA3"},
-        display: {type: String, default: "ABF Family Practice"}
-      },
-      status: "tentative"
-    }, 
-  ]
+  participant: [participantSchema]
 },{timestamps: true});
 
 const Appointments = mongoose.model("Appointment", AppointmentsSchema);
