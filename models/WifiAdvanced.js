1714a093f2d2a1768e8bdae10f2999383278a31f
EricFithian <ef2413@columbia.edu>
2025-07-08 15:30:44 -0400

    Added logs

--- models/WifiAdvanced.js
+++ models/WifiAdvanced.js
@@ -8,21 +8,12 @@ const visitorSchema = new mongoose.Schema({
   password: String
 })
 const WifiAdvancedSchema = new mongoose.Schema({
-  number: {
-    type: Number
-  },
-  vendor: {
-    type: String
-  },
-  startDate: {
-    type: String
-  },
-  endDate: {
-    type: String
-  },
-  duration: {
-    type: Number
-  }, visitors: [visitorSchema]
+  number: Number,
+  vendor: String,
+  startDate: String,
+  endDate: String,
+  duration: Number, 
+  visitors: [visitorSchema]
 },{timestamps: true});
 
 const WifiAdvanced = mongoose.model("WifiAdvanced", WifiAdvancedSchema);
