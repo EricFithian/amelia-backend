35011a480285619124162172a33e9b6378c7321f
EricFithian <ef2413@columbia.edu>
2025-09-12 00:05:15 -0400

    Updated server to edit beneficiary

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/beneficiary', async (req, res) => {
 
 app.get('/beneficiary/:email', async (req, res) => {
     try {
-        const singleBenfit = await Beneficiary.findOne({emailOfPolicyHolder: req.params.emailOfPolicyHolder});
+        const singleBenfit = await Beneficiary.findOne({emailOfPolicyHolder: req.params.email});
         res.status(200).json({beneficiary: singleBenfit});
     } catch(err) {
         res.status(400).json(err);
