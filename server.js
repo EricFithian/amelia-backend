76508b7fc2a103ad39b3b463b9d35ae4219634c0
EricFithian <ef2413@columbia.edu>
2025-11-13 19:43:08 -0500

    Updated routes

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/claim_status/:email', async (req, res) => {
 
 app.get('HoundifyMCP', async (req, res) => {
     try {
-        res.status(200).json({ results: {
+        res.json({ results: {
             "CommandKind": "CarControlCommand",
             "CarControlCommandKind": "CarControlDoorCommand",
             "CommandType": "OpenDoor",
