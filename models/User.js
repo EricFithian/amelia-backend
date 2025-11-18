223e3d186577e8c106edaa44fc360e4d7f27f51c
EricFithian <ef2413@columbia.edu>
2025-11-18 06:02:57 -0500

    Updated schema for users

--- models/User.js
+++ models/User.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 ///////////////////////////////
 // MODELS
 ////////////////////////////////
+
+const claimsSchema = new mongoose.Schema({
+  claimNumber: Number,
+  description: String,
+  status: String
+})
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -20,7 +27,7 @@ const UserSchema = new mongoose.Schema({
   }, phoneNumber: {
     type: String,
     default: "123-867-5309"
-  }
+  }, claims: [claimsSchema]
 },{timestamps: true});
 
 const User = mongoose.model("User", UserSchema);
